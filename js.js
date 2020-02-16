a2222e65a49fed70c56660aa9c111fb29d33f270
Your Name <xfitex>
2020-02-16 17:04:23 -0300

    asd

--- js.js
+++
@@ -1,6 +0,0 @@
-let imgHeader = document.getElementById("imgHeader");
-
-window.addEventListener('scroll', function(){
-let value = window.scrollY;
-imgHeader.style.clipPath = "circle("+ value +"px at center)";
-});
