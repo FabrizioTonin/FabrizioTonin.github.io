4afe69abde9383e6ed7854f846efa2f02f9ac123
Your Name <xfitex>
2020-02-18 11:45:39 -0300

    cambios profile

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 let imgHeader = document.getElementById("imgHeader");
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', () => {
 let value = window.scrollY;
 imgHeader.style.clipPath = "circle("+ value +"px at center)";
 });
